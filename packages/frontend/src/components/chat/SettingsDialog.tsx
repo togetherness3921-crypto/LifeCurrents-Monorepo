3fa626eb7e81f0f2f0464b0430aa9a9f084de138
GitHub Action <action@github.com>
2025-10-31 18:26:57 -0500

    Auto-sync

--- packages/frontend/src/components/chat/SettingsDialog.tsx
+++ packages/frontend/src/components/chat/SettingsDialog.tsx
@@ -416,7 +416,7 @@ const SettingsDialog: React.FC<SettingsDialogProps> = ({ open, onOpenChange, pre
                 onOpenChange(nextOpen);
             }}
         >
-            <DialogContent className="max-w-4xl">
+            <DialogContent className="max-w-4xl max-h-[90vh] flex flex-col overflow-hidden">
                 <DialogHeader>
                     <DialogTitle>Settings</DialogTitle>
                     <DialogDescription>
